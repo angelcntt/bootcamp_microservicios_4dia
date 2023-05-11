94a495fa604bdf20ee373e0632af895a7b32d345
angelcntt <131164580+angelcntt@users.noreply.github.com>
2023-05-11 14:25:57 +0200

    Add files via upload

---
+++ webflux-micro-app/src/main/java/com/nttdata/bootcamp/webfluxmicroapp/controller/Person.java
@@ -0,0 +1,45 @@
+package com.nttdata.bootcamp.webfluxmicroapp.controller;
+
+public class Person {
+	private String firtsname;
+	
+	private String lastname;
+	
+	private Integer age;
+	
+	
+	
+	
+
+	public Person(String firtsname, String lastname, Integer age) {
+		this.firtsname = firtsname;
+		this.lastname = lastname;
+		this.age = age;
+	}
+
+	public String getFirtsname() {
+		return firtsname;
+	}
+
+	public void setFirtsname(String firtsname) {
+		this.firtsname = firtsname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	
+	
+}
